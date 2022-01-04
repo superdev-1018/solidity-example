046d1e4cbb6e557cec55a443a37f4f8561eebf5f
tophusna <husnakormokers@gmail.com>
2022-01-04 11:41:10 +0530

    :bug: fix: null of insertAdjacentHTML

--- assets/js/likes.js
+++ assets/js/likes.js
@@ -2,17 +2,16 @@ const app = document.querySelector("#likes-list");
 const RSS_URL = `https://getpocket.com/users/*em1604085158936836b/feed/read`;
 const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
-
-fetch(PROXY_URL + RSS_URL)
+if (app) {
+    fetch(PROXY_URL + RSS_URL)
     .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
-    console.log(data);
-    const items = data.querySelectorAll("item");
-    let html = ``;
-    items.forEach(el => {
-      var url = new URL(el.querySelector("link").innerHTML);
-      html += `
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+        const items = data.querySelectorAll("item");
+        let html = ``;
+        items.forEach(el => {
+            var url = new URL(el.querySelector("link").innerHTML);
+            html += `
         <article class="LikesItem">
           <h3 class="LikesItem-title size-3 as-link">
             <a class="link-dark" href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
@@ -25,8 +24,8 @@ fetch(PROXY_URL + RSS_URL)
           )}</div>
         </article>
       `;
+        });
+        app.insertAdjacentHTML("beforeend", html);
     });
-    app.insertAdjacentHTML("beforeend", html);
-  });
-
+}
 
